eee620f3b29ec26233a3751c1290cb87783f98a5
Michael Friedman <minorpianokeys@gmail.com>
2023-08-16 21:06:41 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function isW(obj) {
+    return (obj["result"] === "W")
+}
+
+function superbowlWin(array) {
+    const winner = array.find(isW)
+    if (winner === undefined) {
+        return undefined
+    } else {
+        return winner["year"]
+    }
+}
